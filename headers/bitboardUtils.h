7778b3839340c1ab3eb5182e18c0c5c75530d3ad
waffleprinter <dwamsie@gmail.com>
2024-10-14 12:54:48 -0400

    Added basic one square moves

--- headers/bitboardUtils.h
+++ headers/bitboardUtils.h
@@ -6,7 +6,22 @@
 #define U64 uint64_t
 
 class bitboardUtils {
+public:
+    const static U64 notAFile = 0xfefefefefefefefe; // ~0x0101010101010101
+    const static U64 notHFile = 0x7f7f7f7f7f7f7f7f; // ~0x8080808080808080
 
+    static U64 nortOne(U64 b) {return b << 8;};
+    static U64 soutOne(U64 b) {return b >> 8;};
+
+    // Mask out A file to avoid wrap around from H file
+    static U64 eastOne(U64 b) {return (b << 1) & notAFile;};
+    static U64 noEaOne(U64 b) {return (b << 9) & notAFile;};
+    static U64 soEaOne(U64 b) {return (b >> 7) & notAFile;};
+
+    // Mask out H file to avoid wrap around from A file
+    static U64 westOne(U64 b) {return (b >> 1) & notHFile;};
+    static U64 noWeOne(U64 b) {return (b << 7) & notHFile;};
+    static U64 soWeOne(U64 b) {return (b >> 9) & notHFile;};
 };
 
 
