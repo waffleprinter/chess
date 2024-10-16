b461986aa48d854c88d972209e55ed23e71c2119
waffleprinter <dwamsie@gmail.com>
2024-10-16 12:14:34 -0400

    added accessor functions

--- headers/move_gen.h
+++ headers/move_gen.h
@@ -15,6 +15,10 @@ private:
 
     static std::array<u64, 64> generate_knight_lookup();
     static std::array<u64, 64> generate_king_lookup();
+
+public:
+    static u64 get_knight_attacks(u64 knight_square, u64 occupied) { return knight_lookup[knight_square] & ~occupied; };
+    static u64 get_king_attacks(u64 king_square, u64 occupied) { return king_lookup[king_square] & ~occupied; };
 };
 
 #endif //CHESS_MOVE_GEN_H
