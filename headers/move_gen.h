d06e7e166efee28d85b3ba8fc2f56d7cc83e046f
waffleprinter <dwamsie@gmail.com>
2024-10-16 12:10:42 -0400

    moved function definitions to source file

--- headers/move_gen.h
+++ headers/move_gen.h
@@ -3,58 +3,18 @@
 
 #include <cstdint>
 #include <array>
+#include <bitboard.h>
 #include <bitboard_utils.h>
 
 typedef uint64_t u64;
 
 class move_gen {
 private:
-    static std::array<u64, 64> generate_knight_lookup() {
-        std::array<u64, 64> lookup = {};
-        u64 knight_position = 0x0000000000000001;
-
-        for (int i = 0; i < 64; i++) {
-            u64 attacks = bitboard_utils::north_north_east(knight_position);
-            attacks |= bitboard_utils::north_east_east(knight_position);
-            attacks |= bitboard_utils::south_east_east(knight_position);
-            attacks |= bitboard_utils::south_south_east(knight_position);
-
-            attacks |= bitboard_utils::north_north_west(knight_position);
-            attacks |= bitboard_utils::north_west_west(knight_position);
-            attacks |= bitboard_utils::south_west_west(knight_position);
-            attacks |= bitboard_utils::south_south_west(knight_position);
-
-            lookup[i] = attacks;
-            knight_position <<= 1;
-        }
-
-        return lookup;
-    };
-
-    static std::array<u64, 64> generate_king_lookup() {
-        std::array<u64, 64> lookup = {};
-
-        for (int i = 0; i < 64; i++) {
-            u64 king_position = 1ULL << i;
-
-            u64 attacks = bitboard_utils::north_one(king_position);
-            attacks |= bitboard_utils::north_east_one(king_position);
-            attacks |= bitboard_utils::east_one(king_position);
-            attacks |= bitboard_utils::south_east_one(king_position);
-            attacks |= bitboard_utils::south_one(king_position);
-            attacks |= bitboard_utils::south_west_one(king_position);
-            attacks |= bitboard_utils::west_one(king_position);
-            attacks |= bitboard_utils::north_west_one(king_position);
-
-            lookup[i] = attacks;
-        }
-
-        return lookup;
-    };
-
-public:
     static std::array<u64, 64> knight_lookup;
     static std::array<u64, 64> king_lookup;
+
+    static std::array<u64, 64> generate_knight_lookup();
+    static std::array<u64, 64> generate_king_lookup();
 };
 
 #endif //CHESS_MOVE_GEN_H
--- src/move_gen.cpp
+++ src/move_gen.cpp
@@ -2,3 +2,47 @@
 
 std::array<u64, 64> move_gen::knight_lookup = move_gen::generate_knight_lookup();
 std::array<u64, 64> move_gen::king_lookup = move_gen::generate_king_lookup();
+
+std::array<u64, 64> move_gen::generate_knight_lookup() {
+    std::array<u64, 64> lookup = {};
+
+    for (int i = 0; i < 64; i++) {
+        u64 knight_position = 1ULL << i;
+
+        u64 attacks = bitboard_utils::north_north_east(knight_position);
+        attacks |= bitboard_utils::north_east_east(knight_position);
+        attacks |= bitboard_utils::south_east_east(knight_position);
+        attacks |= bitboard_utils::south_south_east(knight_position);
+
+        attacks |= bitboard_utils::north_north_west(knight_position);
+        attacks |= bitboard_utils::north_west_west(knight_position);
+        attacks |= bitboard_utils::south_west_west(knight_position);
+        attacks |= bitboard_utils::south_south_west(knight_position);
+
+        lookup[i] = attacks;
+    }
+
+    return lookup;
+};
+
+std::array<u64, 64> move_gen::generate_king_lookup() {
+    std::array<u64, 64> lookup = {};
+
+    for (int i = 0; i < 64; i++) {
+        u64 king_position = 1ULL << i;
+
+        u64 attacks = bitboard_utils::north_one(king_position);
+        attacks |= bitboard_utils::north_east_one(king_position);
+        attacks |= bitboard_utils::east_one(king_position);
+        attacks |= bitboard_utils::south_east_one(king_position);
+
+        attacks |= bitboard_utils::south_one(king_position);
+        attacks |= bitboard_utils::south_west_one(king_position);
+        attacks |= bitboard_utils::west_one(king_position);
+        attacks |= bitboard_utils::north_west_one(king_position);
+
+        lookup[i] = attacks;
+    }
+
+    return lookup;
+};
