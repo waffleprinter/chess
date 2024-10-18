93c245f8e08f18cd4af90e76ac180fe8adf037b1
waffleprinter <dwamsie@gmail.com>
2024-10-18 01:19:41 -0400

    added pawn_push_lookup, generate_pawn_push_lookup(), and get_pawn_pushes(), along with their implementations

--- headers/move_gen.h
+++ headers/move_gen.h
@@ -10,17 +10,19 @@ typedef uint64_t u64;
 
 class move_gen {
 private:
-    // static std::array<std::array<u64, 64>, 2> pawn_push_lookup;
+    static std::array<std::array<u64, 64>, 2> pawn_push_lookup;
     static std::array<std::array<u64, 64>, 2> pawn_attack_lookup;
     static std::array<u64, 64> knight_lookup;
     static std::array<u64, 64> king_lookup;
 
+    static std::array<std::array<u64, 64>, 2> generate_pawn_push_lookup();
     static std::array<std::array<u64, 64>, 2> generate_pawn_attack_lookup();
     static std::array<u64, 64> generate_knight_lookup();
     static std::array<u64, 64> generate_king_lookup();
 
 public:
-    static u64 get_pawn_attacks(board::enum_color color, board::square square) {return pawn_attack_lookup[color][square];}
+    static u64 get_pawn_pushes(board::enum_color color, board::square square) {return pawn_push_lookup[color][square];}
+    static u64 get_pawn_attacks(board::enum_color color, board::square square) {return pawn_attack_lookup[color][square];} // Does not include en passant
     static u64 get_knight_attacks(board::square square) {return knight_lookup[square];}
     static u64 get_king_attacks(board::square square) {return king_lookup[square];}
 };
--- src/move_gen.cpp
+++ src/move_gen.cpp
@@ -1,8 +1,43 @@
 #include "move_gen.h"
 
+std::array<std::array<u64, 64>, 2> move_gen::pawn_push_lookup = move_gen::generate_pawn_push_lookup();
 std::array<std::array<u64, 64>, 2> move_gen::pawn_attack_lookup = move_gen::generate_pawn_attack_lookup();
 std::array<u64, 64> move_gen::knight_lookup = move_gen::generate_knight_lookup();
 std::array<u64, 64> move_gen::king_lookup = move_gen::generate_king_lookup();
+
+std::array<std::array<u64, 64>, 2> move_gen::generate_pawn_push_lookup() {
+    std::array<u64, 64> white_lookup = {};
+
+    for (int i = 0; i < 64; i++) {
+        u64 white_pawn_position = 1ULL << i;
+
+        u64 pushes = bitboard_utils::north_one(white_pawn_position);
+
+        bool has_not_moved = ((1ULL << 8) <= white_pawn_position) && (white_pawn_position <= (1ULL << 15));
+        if (has_not_moved)
+            pushes |= bitboard_utils::north_one(pushes);
+
+        white_lookup[i] = pushes;
+    }
+
+    std::array<u64, 64> black_lookup = {};
+
+    for (int i = 0; i < 64; i++) {
+        u64 black_pawn_position = 1ULL << i;
+
+        u64 pushes = bitboard_utils::south_one(black_pawn_position);
+
+        bool has_not_moved = ((1ULL << 48) <= black_pawn_position) && (black_pawn_position <= (1ULL << 55));
+        if (has_not_moved)
+            pushes |= bitboard_utils::south_one(pushes);
+
+        black_lookup[i] = pushes;
+    }
+
+    std::array<std::array<u64, 64>, 2> lookup = {white_lookup, black_lookup};
+
+    return lookup;
+}
 
 std::array<std::array<u64, 64>, 2> move_gen::generate_pawn_attack_lookup() {
     std::array<u64, 64> white_lookup = {};
@@ -75,3 +110,5 @@ std::array<u64, 64> move_gen::generate_king_lookup() {
 
     return lookup;
 }
+
+
