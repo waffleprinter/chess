ff1e1330fd494701a13ddb8fa97485dffc75f11e
waffleprinter <dwamsie@gmail.com>
2024-10-18 21:14:04 -0400

    got rid of faulty get_pawn_pushes() function

--- headers/move_gen.h
+++ headers/move_gen.h
@@ -10,21 +10,18 @@ typedef uint64_t u64;
 
 class move_gen {
 private:
-    static std::array<std::array<u64, 64>, 2> pawn_push_lookup;
     static std::array<std::array<u64, 64>, 2> pawn_attack_lookup;
     static std::array<u64, 64> knight_lookup;
     static std::array<u64, 64> king_lookup;
 
-    static std::array<std::array<u64, 64>, 2> generate_pawn_push_lookup();
     static std::array<std::array<u64, 64>, 2> generate_pawn_attack_lookup();
     static std::array<u64, 64> generate_knight_lookup();
     static std::array<u64, 64> generate_king_lookup();
 
 public:
-    static u64 get_pawn_pushes(board::enum_color color, board::square square) {return pawn_push_lookup[color][square];}
-    static u64 get_pawn_attacks(board::enum_color color, board::square square) {return pawn_attack_lookup[color][square];} // Does not include en passant
-    static u64 get_knight_attacks(board::square square) {return knight_lookup[square];}
-    static u64 get_king_attacks(board::square square) {return king_lookup[square];}
+    static u64 get_pawn_attacks(board::enum_color color, board::enum_square square, board::enum_piece opponent_pieces) {return pawn_attack_lookup[color][square] & opponent_pieces;}
+    static u64 get_knight_attacks(board::enum_square square, board::enum_piece allied_pieces) {return knight_lookup[square] & ~allied_pieces;}
+    static u64 get_king_attacks(board::enum_square square, board::enum_piece allied_pieces) {return king_lookup[square] & ~allied_pieces;}
 };
 
 #endif //CHESS_MOVE_GEN_H
